d5d068d2e92c773eee7008b7400d31f4a49f96c3
Kaan <Kaan_k193@hotmail.com>
2017-08-24 21:59:29 +0300

    Adjust entry point to class changes

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const logger = require('koa-logger');
 const config = require('config');
 const mongoose = require('mongoose');
 const Manager = require('./libs/manager');
+const Bot = require('./libs/bot');
+const ModuleCore = require('./libs/ModuleCore');
 
 mongoose.Promise = require('bluebird');
 mongoose.connect(process.env.DB_LINK || config.get('database.link'));
 
 Bot.once('connected', () => {
-  const manager = new Manager(Bot);
-
-  manager.listen();
+  Bot.on('manager_message', () => console.log('heyyyy'));
 });
 
-module.exports = app;
+
+module.exports = {
+  Manager,
+  Bot,
+  ModuleCore
+};
